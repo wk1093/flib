f0344e6792615a7ecdd4e8a2f4196f6a8587450d
wk1093 <wyattk1093@gmail.com>
2023-01-05 17:50:30 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <cstdio>
 
 int main(void) {
+    // fracti can be replaced with fract or frac and the answers will be identical
+    // The internal workings are different, and some are faster and slower, and some will not work with bigger numbers.
     fracti a(20000, 300);
     fracti b(10000, 200);
     fracti c = a * b;
